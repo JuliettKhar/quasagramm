6de7bbd6a8c2554cc7077fac08c20ee17a843240
Julia Kharlamova <juliettkhar@gmail.com>
2021-05-10 17:52:52 +0300

    finish camera functionality

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,10 +1,33 @@
 <template>
   <q-page class="container-camera q-pa-md">
     <div class="camera-frame q-pa-md">
-      <img src="https://cdn.quasar.dev/img/parallax1.jpg" alt="" class="full-width">
+      <video
+        ref="video"
+        class="full-width"
+        autoplay
+        v-show="!imageCaptured"/>
+      <canvas ref="canvas" class="full-width" height="240" v-show="imageCaptured"/>
     </div>
     <div class="text-center q-pa-md">
-      <q-btn round color="grey-10" icon="eva-camera"/>
+      <q-btn
+        v-if="hasCameraSupports"
+        round
+        color="grey-10"
+        icon="eva-camera"
+        @click="captureImage"
+      />
+      <q-file
+        v-else
+        outlined
+        v-model="imageUpload"
+        label="Choose an image"
+        accept="image/*"
+        @input="captureImageFallback"
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
       <div class="row justify-center q-pa-md">
         <q-input v-model="post.caption" label="Caption" class="col col-sm-6" dense/>
       </div>
@@ -23,7 +46,10 @@
 </template>
 
 <script>
-import { uid } from 'quasar'
+import {uid} from 'quasar'
+
+require('md-gum-polyfill');
+
 export default {
   name: "PageCamera",
   data() {
@@ -35,6 +61,85 @@ export default {
         location: "",
         photo: null
       },
+      imageCaptured: false,
+      hasCameraSupports: true,
+      imageUpload: []
+    }
+  },
+  methods: {
+    initCamera() {
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(stream => {
+        this.$refs.video.srcObject = stream;
+      }).catch((e) => {
+        this.hasCameraSupports = false
+      })
+    },
+    captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      let context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+      this.disableCamera();
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      const byteString = atob(dataURI.split(',')[1]);
+
+      // separate out the mime component
+      const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+      // write the bytes of the string to an ArrayBuffer
+      const ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      const ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      return new Blob([ab], {type: mimeString});
+
+    },
+    captureImageFallback(file) {
+      this.post.photo = file
+      let canvas = this.$refs.canvas;
+      let context = canvas.getContext('2d');
+
+      const reader = new FileReader();
+      reader.onload = event => {
+        let img = new Image()
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0)
+          this.imageCaptured = true;
+        }
+        img.src = event.target.result;
+      }
+      reader.readAsDataURL(file)
+    },
+    disableCamera() {
+      this.$refs.video.srcObject.getTracks().forEach(track => track.stop());
+    }
+
+  },
+  mounted() {
+    this.initCamera();
+  },
+  beforeDestroy() {
+    if(this.hasCameraSupports) {
+      this.disableCamera();
     }
   }
 }

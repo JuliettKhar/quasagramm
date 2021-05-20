3b8779b7e903844d3360777eb046df1c64194563
Julia Kharlamova <juliettkhar@gmail.com>
2021-05-20 22:30:02 +0300

    add cash strategies

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -4,7 +4,39 @@
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
 
+import {StaleWhileRevalidate} from 'workbox-strategies';
+import {CacheFirst} from 'workbox-strategies';
+import {NetworkFirst} from 'workbox-strategies';
+
 import { precacheAndRoute } from 'workbox-precaching';
+import {registerRoute} from 'workbox-routing';
+import {ExpirationPlugin} from 'workbox-expiration';
+import {CacheableResponsePlugin} from 'workbox-cacheable-response';
+
+registerRoute(
+  ({ url }) => url.host.startsWith('fonts.g'),
+  new CacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 30,
+      }),
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  ({url}) => url.pathname.startsWith('/posts'),
+  new NetworkFirst()
+);
+
+registerRoute(
+  ({url}) => url.href.startsWith('http'),
+  new StaleWhileRevalidate()
+);
 
 // Use with precache injection
 precacheAndRoute(self.__WB_MANIFEST);
